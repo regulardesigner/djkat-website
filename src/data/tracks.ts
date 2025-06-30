677114411abc596c4f2955c2ce57d1c141002f83
damien_tailhades <damien.tailhades@nextory.com>
2025-06-30 15:40:01 +0200

    ✨ Update tracks data by removing outdated entries and retaining relevant tracks for improved content quality.

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -4,10 +4,10 @@ interface Track {
 }
 
 export const tracks: Track[] = [
+  { id: "2121110145", title: "JUSTE LIKE SUNDAY" },
+  { id: "2116437954", title: "Fafa Monteco - Good Time (Kat Rework)" },
+  { id: "2113816401", title: "Disco King" },
+  { id: "2111428011", title: "Loop This Mind" },
+  { id: "2110336818", title: "Proud French Touch" },
   { id: "1985596363", title: "So so beautilful" },
-  { id: "2030050792", title: "French Cocktail" },
-  { id: "2025117380", title: "The Tower" },
-  { id: "1997397563", title: "Feel so good" },
-  { id: "1955115635", title: "The starry sky" },
-  { id: "1938287549", title: "The knight" },
 ];
