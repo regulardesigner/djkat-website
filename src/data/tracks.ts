d137b435fcb959cfbe2fb1dfd70c344d39a10f3a
damien_tailhades <damien.tailhades@nextory.com>
2025-07-18 22:02:32 +0200

    ✨ Move "Proud French Touch" track entry to maintain unique titles in the list.

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -4,10 +4,10 @@ interface Track {
 }
 
 export const tracks: Track[] = [
+  { id: "2110336818", title: "Proud French Touch" },
   { id: "2129269977", title: "Filtered Love" },
   { id: "2116437954", title: "Fafa Monteco - Good Time (Kat Rework)" },
   { id: "2113816401", title: "Disco King" },
   { id: "2111428011", title: "Loop This Mind" },
-  { id: "2110336818", title: "Proud French Touch" },
   { id: "1985596363", title: "So so beautilful" },
 ];
